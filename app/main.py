79731601953e2ef9b74637f5c7e1f0102aa77d3f
Yevheniy <Genya421@gmail.com>
2025-01-19 20:49:41 +0200

    del some

--- app/main.py
+++ app/main.py
@@ -16,41 +16,6 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-# origins = [
-#     "http://localhost.tiangolo.com",
-#     "https://localhost.tiangolo.com",
-#     "http://localhost",
-#     "http://localhost:8080",
-#     '127.0.0.1'
-# ]
-#
-#
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
-
-
-# @app.on_event("startup")
-# def on_startup():
-#     create_db_and_tables()
-
-app.include_router(post.router)
-app.include_router(user.router)
-app.include_router(auth.router)
-app.include_router(vote.router)
-#
-# @app.get("/")
-# def read_root():
-#     return {"message": "Hello World"}
-#
-#
-#
-
-
 origins = ["http://localhost",
            "http://localhost:8080",
            "http://127.0.0.1:8000",
@@ -66,6 +31,18 @@ app.add_middleware(
 )
 
 
+app.include_router(post.router)
+app.include_router(user.router)
+app.include_router(auth.router)
+app.include_router(vote.router)
+
 @app.get("/")
-async def main():
+def read_root():
     return {"message": "Hello World"}
+
+
+
+
+
+
+
